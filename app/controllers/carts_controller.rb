f675f41c0dae52036e3f1ec6cbaf9881d4d021f8
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2013-11-29 18:22:52 +0200

    error processing on wrong card id

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  #before_action :set_cart, only: [:show, :edit, :update, :destroy]
 
+  before_action only: [:show, :edit, :update, :destroy]
   # GET /carts
   # GET /carts.json
   def index
@@ -10,7 +11,19 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @cart }
+      end
+    end
   end
+
 
   # GET /carts/new
   def new
