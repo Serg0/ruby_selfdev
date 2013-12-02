79498c7d10757a54c8d4d2ba6437a80f132b8553
Serhii Nadolinskyi <serhii.nadolinskyi@gmail.com>
2013-12-02 19:21:34 +0200

    card test in pro

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,4 +4,34 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  fixtures :products
+  fixtures :carts
+
+  test "create card and save" do
+  card = carts(:one)
+
+  assert_not_nil card
+  assert_not_nil card.id
+
+  product_to_add = products(:ruby)
+
+  assert_equal 2, card.line_items.size,
+
+  card.add_product(product_to_add.id, product_to_add.price)
+
+  assert_equal 3, card.line_items.size
+
+  card.add_product(product_to_add.id, product_to_add.price)
+
+  assert_not_empty card.line_items
+
+  #card.add_product 1 2
+
+
+
+  end
+
+
+
 end
